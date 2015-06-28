b8b9924ecf6a9c3b1a1b5e3f35fd82a791c7af47
muminoff <smuminov@gmail.com>
2015-06-29 00:31:38 +0900

    Migrated from ntwitter to twit

--- server.js
+++ server.js
@@ -1,26 +1,16 @@
-var twitter = require('ntwitter'),
-    sqlite3 = require('sqlite3').verbose(),
+var twitter = require('twit'),
     twitterCredentials = require('./credentials.json');
 
-var db = new sqlite3.Database('tweekent.db');
 twit = new twitter(twitterCredentials);
 
-var tashkent = '69.12597656249999,41.178653972331674,69.4281005859375,41.39741506646461';
+var tashkent = [
+  '69.12597656249999',
+  '41.178653972331674',
+  '69.4281005859375',
+  '41.39741506646461'
+  ];
 
-db.serialize(function() {
-  //TODO: workaround this in future
-  db.run("CREATE TABLE tweets(user VARCHAR, tweet TEXT)");
-  var stmt = db.prepare("INSERT INTO tweets VALUES (?, ?)");
-  twit.stream('statuses/filter', {'locations':tashkent}, function(stream) {
-    stream.on('data', function (tweet) {
-      try {
-        stmt.run(tweet.user.name, tweet.text);
-        console.log(tweet.user.name, "--->", tweet.text);
-        console.log(tweet.geo, tweet.created_at, tweet.id);
-      }
-      catch (e) {
-        console.log("Error occured.", e);
-      }
-    });
-  });
+var stream = twit.stream('statuses/filter', {locations:tashkent});
+stream.on('tweet', function (tweet) {
+  console.log(tweet.text);
 });
